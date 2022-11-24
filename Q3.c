21c408276d8d6152c776ec063e00e737a28079db
rajorshi1 <108346962+rajorshi1@users.noreply.github.com>
2022-11-24 23:24:14 +0530

    Update Q3.c

--- Q3.c
+++ Q3.c
@@ -7,19 +7,6 @@
 
 #define kernel_2D_memcpy 451
 
-void check_equal(float** lhs, float** rhs, int size) {
-    int i;
-    for (i = 0; i < size; i++) {
-        for (int j=0; j<size; j++) {
-            if (lhs[i][j] != rhs[i][j]) {
-                printf("Failed");
-                return;
-            }
-        }
-    }
-    printf("Message : Success LHS = RHS \n");
-}
-
 int main() {
     float MAT1[4][4] = {{1.0,2.0,3.0, 4.0},{3.0,4.0,5.0, 6.0},{5.0,6.0,7.0, 8.0},{7.0,8.0,9.0, 10.0}};
     float MAT2[4][4] = {{1.0,1.0,1.0,1.0},{1.0,1.0,1.0,1.0},{1.0,1.0,1.0,1.0},{1.0,1.0,1.0,1.0}};
@@ -27,7 +14,14 @@ int main() {
     sys_call_status = syscall(kernel_2D_memcpy, MAT1, MAT2);
     if (sys_call_status != EFAULT) {
         printf("Message : System Call 452 successfuly invoked \n");
-        check_equal(MAT1, MAT2, 4);
     }
+    for (int i = 0; i < 4; i++) {
+        for (int j=0; j<4; j++) {
+            if (MAT1[i][j] != MAT2[i][j]) {
+                printf("Failed");
+            }
+        }
+    }
+    printf("Message : Success LHS = RHS \n");
     return 0;
 }
